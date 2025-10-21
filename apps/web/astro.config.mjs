18356be7812c3cb8e9043021cee5f73fb8aad59d
Achmad F. Ibrahim <acfatah@gmail.com>
2025-10-21 12:33:33 +0800

    feat: add Astro Starlight

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import starlight from '@astrojs/starlight'
 import vue from '@astrojs/vue'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -10,15 +11,36 @@ export default defineConfig({
     vue({
       template: {
         compilerOptions: {
-          // ...
+        // ...
         },
       },
     }),
+
+    starlight({
+      title: 'Bun Ark Vue',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      customCss: ['./src/styles/global.css'],
+    }),
   ],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
+
     resolve: {
       alias: {
         '@/': new URL('./src/', import.meta.url).pathname,
