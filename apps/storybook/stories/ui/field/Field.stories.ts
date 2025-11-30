8bb80cdc2baabf951b4fb2fa896491d6aafb92c4
Achmad F. Ibrahim <acfatah@gmail.com>
2025-11-30 09:52:13 +0800

    feat(storybook): add Field stories

---
+++ apps/storybook/stories/ui/field/Field.stories.ts
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+import { html } from 'common-tags'
+
+import {
+  FieldDescription,
+  FieldError,
+  FieldInput,
+  FieldLabel,
+  FieldRequiredIndicator,
+  FieldRoot,
+} from '@/components/ui/field'
+import { registryItem } from '@/components/ui/field/_registry'
+
+import FieldDefaultStory from './FieldDefaultStory.vue'
+import FieldDefaultSource from './FieldDefaultStory.vue?raw'
+
+const meta = {
+  title: 'Components/Field',
+  component: FieldRoot,
+  subcomponents: {
+    FieldLabel,
+    FieldInput,
+    FieldDescription,
+    FieldError,
+    FieldRequiredIndicator,
+  },
+  tags: ['autodocs'],
+
+  parameters: {
+    docs: {
+      description: {
+        component: registryItem.description,
+      },
+    },
+  },
+
+  args: {
+    required: true,
+    invalid: false,
+    disabled: false,
+  },
+
+  argTypes: {
+    required: { control: 'boolean' },
+    invalid: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+  },
+} satisfies Meta<typeof FieldRoot>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: FieldDefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FieldDefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <FieldDefaultStory v-bind="args" />
+    `,
+  }),
+}
---
+++ apps/storybook/stories/ui/field/FieldDefaultStory.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Field } from '@/components/ui/field'
+
+const props = withDefaults(defineProps<{
+  disabled?: boolean
+  invalid?: boolean
+  required?: boolean
+}>(), {
+  disabled: false,
+  invalid: false,
+  required: true,
+})
+</script>
+
+<template>
+  <!-- Can be grouped using Fieldset.Group -->
+  <div data-part="group" class="flex flex-col gap-6">
+    <Field.Root
+      :disabled="props.disabled"
+      :invalid="props.invalid"
+      :required="props.required"
+    >
+      <Field.Label for="username">
+        Username<Field.RequiredIndicator />
+      </Field.Label>
+      <Field.Input id="username" type="text" placeholder="Max Leiter" />
+      <Field.Description>
+        Choose a unique username for your account.
+      </Field.Description>
+      <Field.Error v-if="props.invalid">
+        Username is already taken.
+      </Field.Error>
+    </Field.Root>
+
+    <Field.Root
+      :disabled="props.disabled"
+      :invalid="props.invalid"
+      :required="props.required"
+    >
+      <Field.Label for="password">
+        Password<Field.RequiredIndicator />
+      </Field.Label>
+      <Field.Description>
+        Must be at least 8 characters long.
+      </Field.Description>
+      <Field.Input id="password" type="password" placeholder="********" />
+      <Field.Error v-if="props.invalid">
+        Password must be at least 8 characters.
+      </Field.Error>
+    </Field.Root>
+  </div>
+</template>
