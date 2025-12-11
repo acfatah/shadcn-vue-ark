54939a6b78746b1d8c70e0802d56bce5376f8b58
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-12 07:29:53 +0800

    feat(storybook): add Avatar stories and default story component

---
+++ apps/storybook/stories/ui/avatar/Avatar.stories.ts
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+import { html } from 'common-tags'
+
+import {
+  AvatarFallback,
+  AvatarImage,
+  AvatarRoot,
+} from '@/components/ui/avatar'
+import { registryItem } from '@/components/ui/avatar/_registry'
+import AvatarDefaultStory from './AvatarDefaultStory.vue'
+import AvatarDefaultSource from './AvatarDefaultStory.vue?raw'
+
+const meta = {
+  title: 'Components/Avatar',
+  component: AvatarRoot,
+  subcomponents: {
+    AvatarImage,
+    AvatarFallback,
+  },
+  tags: ['autodocs'],
+
+  parameters: {
+    docs: {
+      description: {
+        component: registryItem.description,
+      },
+    },
+  },
+} satisfies Meta<typeof AvatarRoot>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: AvatarDefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { AvatarDefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <AvatarDefaultStory v-bind="args" />
+    `,
+  }),
+}
---
+++ apps/storybook/stories/ui/avatar/AvatarDefaultStory.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Avatar } from '@/components/ui/avatar'
+</script>
+
+<template>
+  <div class="flex flex-row flex-wrap items-center gap-12">
+    <Avatar.Root>
+      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+      <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+
+    <Avatar.Root class="rounded-lg">
+      <Avatar.Image src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
+      <Avatar.Fallback>ER</Avatar.Fallback>
+    </Avatar.Root>
+
+    <div
+      class="
+        flex -space-x-2
+        *:data-[scope=avatar]:ring-2 *:data-[scope=avatar]:ring-background
+        *:data-[scope=avatar]:grayscale
+      "
+    >
+      <Avatar.Root>
+        <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+      </Avatar.Root>
+
+      <Avatar.Root>
+        <Avatar.Image src="https://github.com/leerob.png" alt="@leerob" />
+        <Avatar.Fallback>LR</Avatar.Fallback>
+      </Avatar.Root>
+
+      <Avatar.Root>
+        <Avatar.Image
+          src="https://github.com/evilrabbit.png"
+          alt="@evilrabbit"
+        />
+        <Avatar.Fallback>ER</Avatar.Fallback>
+      </Avatar.Root>
+    </div>
+  </div>
+</template>
