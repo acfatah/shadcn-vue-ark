ac387e463d8a55cd8af95774180183511ceeedab
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-13 17:24:38 +0800

    fix(registry): add native invalid state handling to UrlInput component

--- packages/registry/src/components/ui/input/UrlInput.vue
+++ packages/registry/src/components/ui/input/UrlInput.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
 import type {
   Emits as PrimitiveInputEmits,
@@ -12,13 +13,21 @@ interface Emits extends PrimitiveInputEmits {}
 const props = defineProps<Props>()
 const emits = defineEmits<Emits>()
 const forwardedProps = useForwardPropsEmits(props, emits)
+const nativeInvalid = ref(false)
+
+function handleInvalid(_event: Event) {
+  nativeInvalid.value = true
+}
 </script>
 
 <template>
   <PrimitiveInput
     scope="url-input"
     type="url"
+    :aria-invalid="nativeInvalid"
     :class="props.class"
     v-bind="forwardedProps"
+    @invalid="handleInvalid"
+    @input="nativeInvalid = false"
   />
 </template>
