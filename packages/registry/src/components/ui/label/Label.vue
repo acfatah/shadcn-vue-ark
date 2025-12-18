38ac2d6f863a5f0a322633abe6df64957e9d807f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-19 07:20:49 +0800

    feat(registry): add Label component

---
+++ packages/registry/src/components/ui/label/Label.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { Dynamic } from '@/composables/dynamic'
+import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
+import { cn } from '@/lib/utils'
+
+interface Props {
+  class?: HTMLAttributes['class']
+  asChild?: boolean
+}
+
+const props = defineProps<Props>()
+const delegatedProps = reactiveOmit(props, 'asChild')
+const forwardedProps = useForwardPropsEmits(delegatedProps)
+</script>
+
+<template>
+  <component
+    :is="props.asChild ? Dynamic : 'label'"
+    data-scope="label"
+    v-bind="forwardedProps"
+    :class="cn(
+      `
+        group/field-label peer/field-label flex w-fit items-center gap-2 text-sm leading-snug
+        font-medium select-none
+        group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50
+        group-data-[disabled=true]/field:opacity-50
+        peer-disabled:cursor-not-allowed peer-disabled:opacity-50
+        has-data-[state=checked]:border-primary has-data-[state=checked]:bg-primary/5
+        has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col
+        has-[>[data-slot=field]]:rounded-md has-[>[data-slot=field]]:border
+        *:data-[slot=field]:p-4
+        dark:has-data-[state=checked]:bg-primary/10
+      `,
+      ``,
+      props.class,
+    )"
+  >
+    <slot />
+  </component>
+</template>
---
+++ packages/registry/src/components/ui/label/_registry.ts
@@ -0,0 +1,39 @@
+import type { RegistryItem } from 'shadcn/schema'
+import { html } from 'common-tags'
+
+export const registryItem = {
+  type: 'registry:ui',
+  name: 'label',
+  title: 'Label',
+
+  description: html`
+    Renders an accessible label associated with controls.
+
+    References:
+    - shadcn/ui: https://ui.shadcn.com/docs/components/label
+  `,
+
+  dependencies: [
+    '@vueuse/core',
+  ],
+
+  files: [
+    {
+      path: 'src/composables/dynamic.ts',
+      type: 'registry:file',
+      target: 'src/composables/dynamic.ts',
+    },
+    {
+      path: 'src/composables/use-forward-props.ts',
+      type: 'registry:file',
+      target: 'src/composables/use-forward-props.ts',
+    },
+    {
+      path: 'src/lib/utils.ts',
+      type: 'registry:file',
+      target: 'src/lib/utils.ts',
+    },
+  ],
+} satisfies RegistryItem
+
+export default registryItem
