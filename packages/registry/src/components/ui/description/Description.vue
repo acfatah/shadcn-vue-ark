7db09a7ff96563c462cb7ecd95a49172bd8b0c8f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-19 11:49:32 +0800

    feat(registry): add Description component

---
+++ packages/registry/src/components/ui/description/Description.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { Dynamic } from '@/composables/dynamic'
+import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
+import { cn } from '@/lib/utils'
+
+interface Props {
+  class?: HTMLAttributes['class']
+  asChild?: boolean
+}
+
+const props = defineProps<Props>()
+const delegatedProps = reactiveOmit(props, 'asChild')
+const forwardedProps = useForwardPropsEmits(delegatedProps)
+</script>
+
+<template>
+  <component
+    :is="props.asChild ? Dynamic : 'p'"
+    data-scope="description"
+    v-bind="forwardedProps"
+    :class="cn(
+      `
+        text-sm leading-normal font-normal text-muted-foreground
+        group-has-data-[orientation=horizontal]/field:text-balance
+        last:mt-0
+        nth-last-2:-mt-1
+        [&>a]:underline [&>a]:underline-offset-4
+        [&>a:hover]:text-primary
+        [[data-variant=legend]+&]:-mt-1.5
+      `,
+      props.class,
+    )"
+  >
+    <slot />
+  </component>
+</template>
---
+++ packages/registry/src/components/ui/description/_registry.ts
@@ -0,0 +1,43 @@
+import type { RegistryItem } from 'shadcn/schema'
+import { html } from 'common-tags'
+
+export const registryItem = {
+  type: 'registry:ui',
+  name: 'description',
+  title: 'Description',
+
+  description: html`
+    Provide additional information or context. Typically used to elaborate on an
+    input, title, or heading, offering users more insight into the content or
+    purpose.
+
+    It is usually derived as other component such as <code>Input.Description</code>,
+    <code>Select.Description</code>, etc. to maintain consistent styling across
+    the UI. The differences come from the <code>data-scope</code> and
+    <code>data-part</code> attributes assigned to each component.
+  `,
+
+  dependencies: [
+    '@vueuse/core',
+  ],
+
+  files: [
+    {
+      path: 'src/composables/dynamic.ts',
+      type: 'registry:file',
+      target: 'src/composables/dynamic.ts',
+    },
+    {
+      path: 'src/composables/use-forward-props.ts',
+      type: 'registry:file',
+      target: 'src/composables/use-forward-props.ts',
+    },
+    {
+      path: 'src/lib/utils.ts',
+      type: 'registry:file',
+      target: 'src/lib/utils.ts',
+    },
+  ],
+} satisfies RegistryItem
+
+export default registryItem
