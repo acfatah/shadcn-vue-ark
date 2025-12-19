096b51e0aba765701cb82e91dc3c1028332d21df
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-20 07:48:48 +0800

    feat(storybook): add Description stories

---
+++ apps/storybook/stories/ui/description/Description.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+import { html } from 'common-tags'
+
+import { Description } from '@/components/ui/description'
+import { registryItem } from '@/components/ui/description/_registry'
+import DescriptionDefaultStory from './DescriptionDefaultStory.vue'
+import DescriptionDefaultSource from './DescriptionDefaultStory.vue?raw'
+
+const meta = {
+  title: 'Components/Description',
+  component: Description,
+  tags: ['autodocs'],
+
+  parameters: {
+    docs: {
+      description: {
+        component: registryItem.description,
+      },
+    },
+  },
+} satisfies Meta<typeof Description>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: DescriptionDefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DescriptionDefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DescriptionDefaultStory v-bind="args" />
+    `,
+  }),
+}
---
+++ apps/storybook/stories/ui/description/DescriptionDefaultStory.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Description } from '@/components/ui/description'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+</script>
+
+<template>
+  <Input.Group>
+    <!-- or use "Input.Label" -->
+    <Label>Card number</Label>
+    <Input.Text placeholder="1234 5678 9012 3456" />
+    <!-- or use "Input.Description" component -->
+    <Description>Enter your 16-digit number.</Description>
+  </Input.Group>
+</template>
