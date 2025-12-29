98e6e5793aa6c5b85864a4e6b5a6f1f438bfea96
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-29 18:54:06 +0800

    refactor(registry): update CheckboxGroup component to use useForwardPropsEmits
    - currently unable to extend CheckboxGroupProps. @vue/compiler-sfc give
      failed to resolve extends base type error for Vue v3.3+
      See: [CheckboxGroup](https://github.com/chakra-ui/ark/blob/main/packages/vue/src/components/checkbox/checkbox-group.vue) component.

--- packages/registry/src/components/ui/checkbox/CheckboxGroup.vue
+++ packages/registry/src/components/ui/checkbox/CheckboxGroup.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
 import { Checkbox } from '@ark-ui/vue/checkbox'
+import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
+
+// Unable to extend CheckboxGroupProps.
+// [@vue/compiler-sfc] Failed to resolve extends base type for Vue v3.3+
+// See: https://github.com/chakra-ui/ark/blob/main/packages/vue/src/components/checkbox/checkbox-group.vue
+interface Props {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<Props>()
+const forwardedProps = useForwardPropsEmits(props)
 </script>
 
 <template>
-  <Checkbox.Group v-bind="$attrs">
+  <Checkbox.Group v-bind="forwardedProps">
     <slot />
   </Checkbox.Group>
 </template>
