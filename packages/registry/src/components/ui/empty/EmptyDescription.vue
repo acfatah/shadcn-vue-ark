5477aa254ff00690089fccd14bed69caaccb194c
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-29 11:22:44 +0800

    refactor(registry): update EmptyDescription component to use ark.p and enhance props handling

--- packages/registry/src/components/ui/empty/EmptyDescription.vue
+++ packages/registry/src/components/ui/empty/EmptyDescription.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import type { PolymorphicProps } from '@ark-ui/vue'
 import type { HTMLAttributes } from 'vue'
+import { ark } from '@ark-ui/vue'
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
 import { cn } from '@/lib/utils'
 
-interface Props {
+interface Props extends PolymorphicProps {
   class?: HTMLAttributes['class']
 }
 
 const props = defineProps<Props>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardPropsEmits(delegatedProps)
 </script>
 
 <template>
-  <p
+  <ark.p
+    v-bind="forwardedProps"
     data-scope="empty"
     data-part="description"
     :class="cn(
@@ -23,5 +30,5 @@ const props = defineProps<Props>()
     )"
   >
     <slot />
-  </p>
+  </ark.p>
 </template>
