98e3cde0a3def572aad322f46b95ab9fd9f52cc9
Achmad F. Ibrahim <acfatah@gmail.com>
2025-12-29 19:00:18 +0800

    refactor(registry): update SwitchControl component
    - use useForwardPropsEmits for improved prop handling

--- packages/registry/src/components/ui/switch/SwitchControl.vue
+++ packages/registry/src/components/ui/switch/SwitchControl.vue
@@ -3,6 +3,7 @@ import type { SwitchControlProps } from '@ark-ui/vue/switch'
 import type { HTMLAttributes } from 'vue'
 import { Switch } from '@ark-ui/vue/switch'
 import { reactiveOmit } from '@vueuse/core'
+import { useForwardPropsEmits } from '@/composables/use-forward-props-emits'
 import { cn } from '@/lib/utils'
 
 interface Props extends SwitchControlProps {
@@ -11,11 +12,12 @@ interface Props extends SwitchControlProps {
 
 const props = defineProps<Props>()
 const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardPropsEmits(delegatedProps)
 </script>
 
 <template>
   <Switch.Control
-    v-bind="delegatedProps"
+    v-bind="forwardedProps"
     :class="cn(
       `
         inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent
