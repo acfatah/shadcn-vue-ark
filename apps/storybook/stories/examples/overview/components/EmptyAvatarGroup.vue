67b40a62daad61efcf34abd54455a3b009b4b0b0
Achmad F. Ibrahim <acfatah@gmail.com>
2026-01-03 11:47:20 +0800

    feat(storybook): add EmptyAvatarGroup overview component story

---
+++ apps/storybook/stories/examples/overview/components/EmptyAvatarGroup.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { Avatar } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Empty } from '@/components/ui/empty'
+</script>
+
+<template>
+  <Empty.Root class="flex-none border">
+    <Empty.Header>
+      <Empty.Media>
+        <Avatar.Group>
+          <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+          </Avatar.Root>
+          <Avatar.Root>
+            <Avatar.Image
+              src="https://github.com/maxleiter.png"
+              alt="@maxleiter"
+            />
+            <Avatar.Fallback>LR</Avatar.Fallback>
+          </Avatar.Root>
+          <Avatar.Root>
+            <Avatar.Image
+              src="https://github.com/evilrabbit.png"
+              alt="@evilrabbit"
+            />
+            <Avatar.Fallback>ER</Avatar.Fallback>
+          </Avatar.Root>
+        </Avatar.Group>
+      </Empty.Media>
+
+      <Empty.Title>No Team Members</Empty.Title>
+      <Empty.Description>
+        Invite your team to collaborate on this project.
+      </Empty.Description>
+    </Empty.Header>
+
+    <Empty.Content>
+      <Button size="sm">
+        <Icon icon="lucide:plus" />
+        Invite Members
+      </Button>
+    </Empty.Content>
+  </Empty.Root>
+</template>
