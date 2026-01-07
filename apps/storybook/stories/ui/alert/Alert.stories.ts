d15775a919714b7c74be19f9eff53d38da9329bf
Achmad F. Ibrahim <acfatah@gmail.com>
2026-01-07 13:38:43 +0800

    feat(storybook): add Alert stories

---
+++ apps/storybook/stories/ui/alert/Alert.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+import { html } from 'common-tags'
+
+import { Alert } from '@/components/ui/alert'
+import { registryItem } from '@/components/ui/alert/_registry'
+import AlertDefaultStory from './AlertDefaultStory.vue'
+import AlertDefaultSource from './AlertDefaultStory.vue?raw'
+
+const meta = {
+  title: 'Components/Alert',
+  component: Alert.Root,
+  tags: ['autodocs'],
+
+  parameters: {
+    docs: {
+      description: {
+        component: registryItem.description,
+      },
+    },
+  },
+} satisfies Meta<typeof Alert.Root>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: AlertDefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { AlertDefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <AlertDefaultStory v-bind="args" />
+    `,
+  }),
+}
