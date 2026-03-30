612ba5739a082c7eaaf367d044276cc69001d156
Achmad F. Ibrahim <acfatah@gmail.com>
2026-03-30 10:56:25 +0800

    feat(storybook): add DatePicker stories

---
+++ apps/storybook/stories/ui/date-picker/DatePicker.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+
+import { html } from 'common-tags'
+
+import { DatePicker } from '@/components/ui/date-picker'
+import { registryItem } from '@/components/ui/date-picker/_registry'
+
+import DatePickerDefaultStory from './DatePickerDefaultStory.vue'
+import DatePickerDefaultSource from './DatePickerDefaultStory.vue?raw'
+
+const meta = {
+  title: 'Components/DatePicker',
+  component: DatePicker.Root,
+  tags: ['autodocs'],
+
+  parameters: {
+    docs: {
+      description: {
+        component: registryItem.description,
+      },
+    },
+  },
+} satisfies Meta<typeof DatePicker>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: DatePickerDefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DatePickerDefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DatePickerDefaultStory v-bind="args" />
+    `,
+  }),
+}
---
+++ apps/storybook/stories/ui/date-picker/DatePickerDefaultStory.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { DateValue } from '@internationalized/date'
+
+import { ref } from 'vue'
+
+import { DatePicker } from '@/components/ui/date-picker'
+
+const date = ref<DateValue>()
+</script>
+
+<template>
+  <DatePicker.Root v-model="date">
+    <DatePicker.Trigger />
+    <DatePicker.Content />
+  </DatePicker.Root>
+</template>
