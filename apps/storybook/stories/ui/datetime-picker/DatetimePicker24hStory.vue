65f6c00c34b0c7399b1430781a35a5730a58326e
Achmad F. Ibrahim <acfatah@gmail.com>
2026-06-03 11:56:08 +0800

    feat(storybook): add datetime picker stories

---
+++ apps/storybook/stories/ui/datetime-picker/DatetimePicker24hStory.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { CalendarDateTime } from '@internationalized/date'
+
+import { ref } from 'vue'
+
+import { DatetimePicker } from '@/components/ui/datetime-picker'
+
+const datetime = ref<CalendarDateTime>()
+</script>
+
+<template>
+  <DatetimePicker.Root v-model="datetime" :hour-cycle="24">
+    <DatetimePicker.Trigger />
+    <DatetimePicker.Content />
+  </DatetimePicker.Root>
+</template>
