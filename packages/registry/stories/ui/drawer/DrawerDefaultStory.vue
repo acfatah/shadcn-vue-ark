35c4b1ff1820f23ee6bbc8087a6fa38343e98a21
Achmad F. Ibrahim <acfatah@gmail.com>
2026-06-04 18:06:10 +0800

    refactor(stories): comment out unused data array

--- packages/registry/stories/ui/drawer/DrawerDefaultStory.vue
+++ packages/registry/stories/ui/drawer/DrawerDefaultStory.vue
@@ -5,21 +5,21 @@ import { ref } from 'vue'
 import { Button } from '@/components/ui/button'
 import { Drawer } from '@/components/ui/drawer'
 
-const data = [
-  { goal: 400 },
-  { goal: 300 },
-  { goal: 200 },
-  { goal: 300 },
-  { goal: 200 },
-  { goal: 278 },
-  { goal: 189 },
-  { goal: 239 },
-  { goal: 300 },
-  { goal: 200 },
-  { goal: 278 },
-  { goal: 189 },
-  { goal: 349 },
-]
+// const data = [
+//   { goal: 400 },
+//   { goal: 300 },
+//   { goal: 200 },
+//   { goal: 300 },
+//   { goal: 200 },
+//   { goal: 278 },
+//   { goal: 189 },
+//   { goal: 239 },
+//   { goal: 300 },
+//   { goal: 200 },
+//   { goal: 278 },
+//   { goal: 189 },
+//   { goal: 349 },
+// ]
 
 const goal = ref(350)
 </script>
